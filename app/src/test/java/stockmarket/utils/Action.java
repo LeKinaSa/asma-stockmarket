a6d28784bfc38e82ed18a1fd1bb1163b88378d53
Clara Martins <clara.alves.martins@hotmail.com>
2022-05-03 11:45:38 +0100

    Biggest Discovery Ever

--- app/src/test/java/stockmarket/utils/Action.java
+++ app/src/test/java/stockmarket/utils/Action.java
@@ -1,10 +1,14 @@
 package stockmarket.utils;
 
 import org.junit.jupiter.api.Test;
+
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.MessageTemplate;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class AppTest {
-    @Test void test() {
+    @Test void actionTest() {
         Action action;
 
         action = new Action(ActionType.DAY_OVER, "5");
@@ -18,5 +22,18 @@ class AppTest {
 
         action = Action.toAction("A/3");
         assertNull(action);
+    }
+
+    @Test void messageTemplateTest() {
+        MessageTemplate template = MessageTemplate.and(
+            MessageTemplate.MatchPerformative(ACLMessage.INFORM),
+            MessageTemplate.MatchOntology(ActionType.NEW_DAY.toString())
+        );
+        ACLMessage message = Utils.createMessage(
+            null, ACLMessage.INFORM,
+            ActionType.NEW_DAY.toString(), "1",
+            null, null
+        );
+        assertTrue(template.match(message));
     }
 }

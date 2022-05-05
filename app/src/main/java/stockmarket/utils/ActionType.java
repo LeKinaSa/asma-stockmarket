39df294fcb7d071b085f44e4e6fdc5746e5a8bf7
Clara Martins <clara.alves.martins@hotmail.com>
2022-05-05 10:30:22 +0100

    Updated Possible Action Types

--- app/src/main/java/stockmarket/utils/ActionType.java
+++ app/src/main/java/stockmarket/utils/ActionType.java
@@ -1,11 +1,15 @@
 package stockmarket.utils;
 
 public enum ActionType {
-    START,
-    CHECK_BALANCE,
-    CHECK_STOCK,
-    BUY_STOCK,
-    ORACLE_TIP,
-    DAY_OVER,
-    NEW_DAY
+    START             , // Start a Bank Account or an Entry at the Stock Market
+    CHECK_BALANCE     , // Check the Balance for the Agent's Bank Account
+    TRANSFER_MONEY    , // Transfer Money to Another Agent's Account
+    CHECK_OWNED_STOCK , // Check the Agent's Owned Stock
+    CHECK_STOCK_PRICES, // Check the Current Stock Prices
+    BUY_STOCK         , // Buy / Sell Stock
+    MANAGE_MONEY      , // Place / Remove Money from an Agent's Account (must be Stock Market)
+    DAY_OVER          , // The Agent has concluded its Actions for the Day
+    NEW_DAY           , // New Day has Started
+    ORACLE_TIP        , // Oracle Gives a Tip to an Agent
+    LOAN_MONEY        , // Agent Asks for a Loan to another Agent
 }

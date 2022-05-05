d8208b280ae21a7586e0856b5b2366dbc49c83e2
Clara Martins <clara.alves.martins@hotmail.com>
2022-05-05 11:02:45 +0100

    Transfer Money

--- app/src/main/java/stockmarket/listeners/protocols/BankManager.java
+++ app/src/main/java/stockmarket/listeners/protocols/BankManager.java
@@ -2,11 +2,18 @@ package stockmarket.listeners.protocols;
 
 import java.util.HashMap;
 import java.util.Map;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonSyntaxException;
+
+import jade.domain.FIPAAgentManagement.FailureException;
 import jade.lang.acl.ACLMessage;
 import stockmarket.utils.Action;
 import stockmarket.utils.ActionType;
+import stockmarket.utils.MoneyTransfer;
 
 public class BankManager extends RequestResponder {
+    private Gson gson = new Gson();
     private Map<String, Double> bankAccount = new HashMap<>();
 
     @Override
@@ -26,22 +33,56 @@ public class BankManager extends RequestResponder {
         ActionType actionType = action.getType();
         switch (actionType) {
             case START: {
-                if (!bankAccount.containsKey(agent)) {
-                    double value = 0D;
-                    try {
-                        value = Double.parseDouble(action.getInformation());
-                    }
-                    catch (NumberFormatException ignored) {}
-                    bankAccount.put(agent, value);
-                    return "Started Bank Account";
+                double value = 0D;
+                try {
+                    value = Double.parseDouble(action.getInformation());
                 }
-                return bankAccount.get(agent).toString();
+                catch (NumberFormatException ignored) {}
+
+                // TODO: lock bank
+                if (!bankAccount.containsKey(agent)) {
+                    bankAccount.put(agent, value);
+
+                    // TODO: unlock bank
+                    return "Started Bank Account with " + value + ".";
+                }
+                // TODO: unlock bank
             }
             case CHECK_BALANCE: {
-                return bankAccount.get(agent).toString();
+                // TODO: lock bank
+                Double balance = bankAccount.get(agent);
+                // TODO: unlock bank
+
+                return "Balance is at " + balance + ".";
+            }
+            case TRANSFER_MONEY: {
+                MoneyTransfer transfer;
+                try {
+                    transfer = gson.fromJson(action.getInformation(), MoneyTransfer.class);
+                }
+                catch (JsonSyntaxException e) {
+                    return "Invalid Action: Invalid Transfer";
+                }
+
+                // TODO: lock bank
+                if (!bankAccount.containsKey(transfer.to)) {
+                    // TODO: unlock bank
+                    return "Invalid Action: Unknown Transfer Destination Agent";
+                }
+                if (bankAccount.get(agent) < transfer.amount) {
+                    // TODO: unlock bank
+                    return "Invalid Action: Not Enough Money for Transfer";
+                }
+                bankAccount.put(agent, bankAccount.get(agent) - transfer.amount);
+                bankAccount.put(agent, bankAccount.get(transfer.to) + transfer.amount);
+                
+                double balance = bankAccount.get(agent);
+                // TODO: unlock bank
+
+                return transfer.amount + " transfered. Balance is now at " + balance + ".";
             }
             default: {
-                return "Action Not Supported";
+                return "Invalid Action: Action Not Supported";
             }
         }
     }
---
+++ app/src/main/java/stockmarket/utils/MoneyTransfer.java
@@ -0,0 +1,6 @@
+package stockmarket.utils;
+
+public class MoneyTransfer {
+    public String to;
+    public double amount;
+}

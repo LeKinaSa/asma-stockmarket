c831be3c6f4f767a9a989852c1d5f6477f9e0767
Clara Martins <clara.alves.martins@hotmail.com>
2022-05-07 09:17:45 +0100

    Added Agent Register

--- app/src/main/java/stockmarket/utils/Utils.java
+++ app/src/main/java/stockmarket/utils/Utils.java
@@ -4,10 +4,16 @@ import java.util.Date;
 import java.util.List;
 import jade.core.AID;
 import jade.core.Agent;
+import jade.domain.DFService;
+import jade.domain.FIPAException;
+import jade.domain.FIPANames;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
 import jade.lang.acl.ACLMessage;
 import jade.lang.acl.MessageTemplate;
 
 public class Utils {
+    // Message Related Utilitaries
     public static MessageTemplate getMessageTemplate(String protocol, int performative, ActionType ontology) {
         MessageTemplate template = MessageTemplate.MatchPerformative(performative);
         if (protocol != null) {
@@ -25,6 +31,7 @@ public class Utils {
             List<String> receivers, Date date) {
 
         ACLMessage message = new ACLMessage(performative);
+        message.setLanguage(FIPANames.ContentLanguage.FIPA_SL);
         if (protocol != null) {
             message.setProtocol(protocol);
         }
@@ -90,18 +97,45 @@ public class Utils {
         return "Invalid Action" + message + ".";
     }
 
+    // Yellow Page Utilitaries
+    public static void registerInYellowPages(Agent agent, AgentType type) {
+        try {
+            DFAgentDescription register = new DFAgentDescription();
+            ServiceDescription service = new ServiceDescription();
+
+            service.setName(agent.getLocalName());
+            service.setType(type.toString());
+            service.addLanguages(FIPANames.ContentLanguage.FIPA_SL);
+
+            for (String ontology : type.getOntologies()) {
+                service.addOntologies(ontology);
+            }
+            // TODO: Properties: service.addProperties(new Property("country", "Italy"));
+
+            register.setName(agent.getAID());
+            register.addServices(service);
+
+            DFService.register(agent, register);
+        }
+        catch (FIPAException exception) {
+            exception.printStackTrace();
+        }
+    }
+
+    // Sleep
+    public static void sleep(int seconds) {
+        try {
+            Thread.sleep(seconds * 1000);
+        }
+        catch (InterruptedException ignored) {}
+    }
+
+    // Logs
     public static void log(Agent agent, String message) {
         System.out.println("Agent " + agent.getLocalName() + ": " + message);
     }
 
     public static void log(AID agent, String message) {
         System.out.println("Agent " + agent.getLocalName() + ": " + message);
-    }
-
-    public static void sleep(int seconds) {
-        try {
-            Thread.sleep(seconds * 1000);
-        }
-        catch (InterruptedException ignored) {}
     }
 }

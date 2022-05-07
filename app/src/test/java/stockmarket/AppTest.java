622e9ea6d1098b8fa58eeb42f52ddbabf9db822d
Clara Martins <clara.alves.martins@hotmail.com>
2022-05-07 09:07:00 +0100

    Clear Test Suite

--- app/src/test/java/stockmarket/AppTest.java
+++ app/src/test/java/stockmarket/AppTest.java
@@ -2,8 +2,14 @@ package stockmarket;
 
 import org.junit.jupiter.api.Test;
 
+import jade.lang.acl.ACLMessage;
 import stockmarket.behaviours.managers.messages.DayOverListener;
+import stockmarket.behaviours.managers.messages.MessageListener;
+import stockmarket.behaviours.managers.messages.NewDayListener;
+import stockmarket.utils.Action;
+import stockmarket.utils.ActionType;
 import stockmarket.utils.MoneyTransfer;
+import stockmarket.utils.Utils;
 import static org.junit.jupiter.api.Assertions.*;
 import com.google.gson.Gson;
 
@@ -23,5 +29,29 @@ class AppTest {
     @Test void increaseDayTest() {
         DayOverListener listener = new DayOverListener();
         assertEquals(listener.nextDay(), 1);
+    }
+
+    @Test void actionTest() {
+        Action action;
+
+        action = new Action(ActionType.DAY_OVER, "5");
+        assertEquals(action.getType().toString(), ActionType.DAY_OVER.toString());
+
+        action = Action.toAction("NEW_DAY", "2");
+        assertEquals(action.getType(), ActionType.NEW_DAY);
+        assertEquals(action.getInformation(), "2");
+
+        action = Action.toAction("messageContent", "1");
+        assertNull(action);
+
+        action = Action.toAction("A", "3");
+        assertNull(action);
+    }
+
+    @Test void newDayMessageTest() {
+        MessageListener listener = new NewDayListener();
+        ACLMessage message = Utils.createNewDayMessage(null, 1);
+        System.out.println(message.getOntology());
+        assertTrue(listener.getTemplate().match(message));
     }
 }
